1bae304802ca170970c85667b77a65f1b4720f79
Matt Rogers <mattr@kde.org>
2011-04-17 22:49:54 -0500

    initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Existential::Application.config.secret_token = '535efe10db6addeeba393e16ed78cb29132c9c5ae919464755a40230733a1c0373d935a73f0c5736c2019d908733fefdf416b2428846fe67069fb7075c3e8c78'
